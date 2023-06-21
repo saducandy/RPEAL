0ac8aaeefa82acada912d9e98ffb27ea25387857
saducandy <adoniasandarge@gmail.com>
2023-06-21 11:24:09 +0100

    Creating RPEAL project

---
+++ src/main/java/com/example/rpeal/RpealApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rpeal/RpealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
